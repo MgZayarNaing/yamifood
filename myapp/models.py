74f541cf4c06fc63efe6e16e8de3461c9c0def75
MgZayarNaing <zayarnaing.st@gmail.com>
2024-08-31 10:13:54 +0630

    path

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Imageslider(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.name
