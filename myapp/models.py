7be61b0523355b05bad166330517bbee989e9e45
MgZayarNaing <zayarnaing.st@gmail.com>
2024-08-31 10:50:16 +0630

    image slider

--- myapp/models.py
+++ myapp/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Imageslider(models.Model):
-    name = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/')
+    description = models.TextField()
 
     def __str__(self):
-        return self.name
+        return self.title
