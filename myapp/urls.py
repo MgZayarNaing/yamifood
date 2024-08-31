bbd55a3d7193b58789977ef115f4b5f2a86fd31d
MgZayarNaing <zayarnaing.st@gmail.com>
2024-08-31 11:03:55 +0630

    slider commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home_list, name='home_list'),
+    path('', views.home_list, name='home_list'),
 
 ]
--- yamifood/urls.py
+++ yamifood/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myapp/', include('myapp.urls')),
+    path('', include('myapp.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
