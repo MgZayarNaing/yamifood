29e2a57a34644258c95bb22a5bccf42bbe55f634
MgZayarNaing <zayarnaing.st@gmail.com>
2024-09-03 14:07:08 +0630

    staticfile

--- yamifood/settings.py
+++ yamifood/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = 'static/'
 # STATICFILES_DIRS =  [BASE_DIR / 'static']
 STATICFILES_DIRS =  [os.path.join(BASE_DIR,  'static')]
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL  = '/media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
